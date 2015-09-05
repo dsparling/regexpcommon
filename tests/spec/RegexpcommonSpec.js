f30ef60fb339d40687aad53a3ad1c9fa1486528a
Doug Sparling <doug.sparling@gmail.com>
2015-09-04 23:24:06 -0500

    added ssn specs

--- tests/spec/RegexpcommonSpec.js
+++ tests/spec/RegexpcommonSpec.js
@@ -3,54 +3,72 @@ describe("Regexpcommon", function() {
   var stringValue = 'abc';
   var floatValue = '42.42';
   var romanValue = 'XLII';
+  var ssnValue = '123-45-6789';
 
   describe("numberInteger", function() {
-    it(integerValue + " should match numberInteger", function() {
+    it("'" + integerValue + "' should match numberInteger", function() {
       match = integerValue.match($.regexpCommon('numberInteger'));
       expect(match[0]).not.toBeNull();
     });
 
-    it(integerValue + " should equal " + integerValue, function() {
+    it("'" + integerValue + "' should equal '" + integerValue + "'", function() {
       match = integerValue.match($.regexpCommon('numberInteger'));
       expect(match[0]).toBe(integerValue);
     });
 
-    it(stringValue + " should not match numberInteger", function() {
+    it("'" + stringValue + "' should not match numberInteger", function() {
       match = stringValue.match($.regexpCommon('numberInteger'));
       expect(match).toBeNull();
     });
   });
 
   describe("numberFloat", function() {
-    it(floatValue + " should match numberFloat", function() {
+    it("'" + floatValue + "' should match numberFloat", function() {
       match = floatValue.match($.regexpCommon('numberFloat'));
       expect(match[0]).not.toBeNull();
     });
 
-    it(floatValue + " should equal " + floatValue, function() {
+    it("'" + floatValue + "' should equal '" + floatValue + "'", function() {
       match = floatValue.match($.regexpCommon('numberFloat'));
       expect(match[0]).toBe(floatValue);
     });
 
-    it(stringValue + " should not match numberFloat", function() {
+    it("'" + stringValue + "' should not match numberFloat", function() {
       match = stringValue.match($.regexpCommon('numberFloat'));
       expect(match).toBeNull();
     });
   });
 
   describe("numberRoman", function() {
-    it(romanValue + " should match numberRoman", function() {
+    it("'" + romanValue + "' should match numberRoman", function() {
       match = romanValue.match($.regexpCommon('numberRoman'));
       expect(match[0]).not.toBeNull();
     });
 
-    it(romanValue + " should equal " + romanValue, function() {
+    it("'" + romanValue + "' should equal '" + romanValue + "'", function() {
       match = romanValue.match($.regexpCommon('numberRoman'));
       expect(match[0]).toBe(romanValue);
     });
 
-    it(stringValue + " should not match numberRoman", function() {
+    it("'" + stringValue + "' should not match numberRoman", function() {
       match = stringValue.match($.regexpCommon('numberRoman'));
+      expect(match).toBeNull();
+    });
+  });
+
+  describe("ssn", function() {
+    it("'" + ssnValue + "' should match ssn", function() {
+      match = ssnValue.match($.regexpCommon('ssn'));
+      expect(match[0]).not.toBeNull();
+    });
+
+    it("'" + ssnValue + "' should equal '" + ssnValue + "'", function() {
+      match = ssnValue.match($.regexpCommon('ssn'));
+      expect(match[0]).toBe(ssnValue);
+    });
+
+    it("'" + stringValue + "' should not match ssn", function() {
+      match = stringValue.match($.regexpCommon('ssn'));
       expect(match).toBeNull();
     });
   });
